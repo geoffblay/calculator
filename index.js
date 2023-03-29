9a7bab97cf24d396929773bd2ded42897c46672d
Geoff Blaylock <gblaylock2024@gmail.com>
2023-03-28 21:02:39 -0400

    finish styling

---
+++ index.js
@@ -0,0 +1,33 @@
+const display = document.querySelector(".display-div")
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(x, y, op) {
+    switch (op) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+            return divide(x, y);
+    }
+}
+
+
+
